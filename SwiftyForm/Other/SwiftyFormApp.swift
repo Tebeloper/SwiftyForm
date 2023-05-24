c6b86e8cc8324f194151f48c2fa06688dd595ee4
tebeloper <dimitrisgkarlemos@gmail.com>
2023-05-24 12:53:23 +0300

    initial commit

---
+++ SwiftyForm/Other/SwiftyFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyFormApp.swift
+//  SwiftyForm
+//
+//  Created by Dimitrios Gkarlemos on 24/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ SwiftyForm/Views/MainView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftyForm
+//
+//  Created by Dimitrios Gkarlemos on 24/05/2023.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
