bc16a1b98facf2c5a80d64204a76b5c6274aee68
tebeloper <dimitrisgkarlemos@gmail.com>
2023-05-25 18:26:10 +0300

    fix gitignore

--- SwiftyForm/Models/RegisterModel.swift
+++ SwiftyForm/Models/RegisterModel.swift
@@ -12,12 +12,12 @@ enum AuthenticationError: Error {
     case NetworkError
 }
 
-struct RegisterUser {
+struct RegisterUser: Codable {
     let username: String
     let password: String
 }
 
-struct RegisterResponse {
+struct RegisterResponse: Codable {
     
 }
 
@@ -27,10 +27,42 @@ class RegisterService {
         
         let registerCredentials = RegisterUser(username: username, password: password)
         
-        guard let url = URL(string: "") else {
+        guard let url = URL(string: "http://localhost:3000/register") else {
             completion(.failure(.NetworkError))
             return
         }
         
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = try? JSONEncoder().encode(registerCredentials)
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                return
+            }
+            
+            switch httpResponse.statusCode {
+            case 200:
+                
+                guard let data = data, error == nil else {
+                    completion(.failure(.invalidCredentials))
+                    return
+                }
+                
+                guard let registerResponse = try? JSONDecoder().decode(RegisterResponse.self, from: data) else {
+                    completion(.failure(.NetworkError))
+                    return
+                }
+                
+                completion(.success(registerResponse))
+                
+            case 401:
+                completion(.failure(.invalidCredentials))
+            default:
+                completion(.failure(.NetworkError))
+            }
+        }.resume()
     }
 }
